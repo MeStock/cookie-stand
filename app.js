8c9eb4d42eeee334420191f566d11b29311a2b1d
mestock <MelissaStock44@gmail.com>
2019-03-11 18:59:44 -0700

    Completed all stores

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
-Create an object for 1st location
+Create an object for each location
 Input information about that store
 
 KVPs
@@ -12,7 +12,10 @@ KVPs
 
 Methods
     1. Calculate random number of customers
-    2. Calc Sum
+    2. Calculate average sales per hour
+    3. Calculate total sales per day
+    4. List details in an ordered manner
+    5. Render results onto webpage
 
 */
 
@@ -25,12 +28,80 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//Create object for 1st store
+//Create object for 1st & Pike store
 var pike = {
   location: '1st and Pike',
   minNumCustomers: 23,
   maxNumCustomers: 65,
   avgCookiePerCustomer: 6.3,
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  numCustomersArray: [],
+  avgSalesPerHour: [],
+  
+  numCustomers: calcNumCustomers,
+  avgSales: calcAvgSales,
+  totalSales: calcTotalCookies,
+  listEverything: calcEverything,
+  render: renderResultsToPage,
+};
+
+//Create object for SeaTac Airport store
+var seatacAirport = {
+  location: 'SeaTac Airport',
+  minNumCustomers: 3,
+  maxNumCustomers: 24,
+  avgCookiePerCustomer: 1.2,
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  numCustomersArray: [],
+  avgSalesPerHour: [],
+  
+  numCustomers: calcNumCustomers,
+  avgSales: calcAvgSales,
+  totalSales: calcTotalCookies,
+  listEverything: calcEverything,
+  render: renderResultsToPage,
+};
+
+//Create object for Seattle Center store
+var seattleCenter = {
+  location: 'Seattle Center',
+  minNumCustomers: 11,
+  maxNumCustomers: 38,
+  avgCookiePerCustomer: 3.7,
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  numCustomersArray: [],
+  avgSalesPerHour: [],
+  
+  numCustomers: calcNumCustomers,
+  avgSales: calcAvgSales,
+  totalSales: calcTotalCookies,
+  listEverything: calcEverything,
+  render: renderResultsToPage,
+};
+
+//Create object for Capitol Hill store
+var capHill = {
+  location: 'Capitol Hill',
+  minNumCustomers: 20,
+  maxNumCustomers: 38,
+  avgCookiePerCustomer: 2.3,
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  numCustomersArray: [],
+  avgSalesPerHour: [],
+  
+  numCustomers: calcNumCustomers,
+  avgSales: calcAvgSales,
+  totalSales: calcTotalCookies,
+  listEverything: calcEverything,
+  render: renderResultsToPage,
+};
+
+//Create object for Alki store
+var alki = {
+  location: 'Alki',
+  minNumCustomers: 20,
+  maxNumCustomers: 38,
+  avgCookiePerCustomer: 2.3,
   hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   numCustomersArray: [],
   avgSalesPerHour: [],
@@ -51,12 +122,16 @@ function calcNumCustomers() {
 
 //Create function to calculate everything (#customers, #sales)
 function calcEverything() {
+  console.log(`${this.location} Results`);
   for(var i = 0; i < this.hoursOfOperation.length; i++){
     this.numCustomersArray.push(this.numCustomers());
     this.avgSalesPerHour.push(this.avgSales(i));
-    console.log(this.hoursOfOperation[i] + ': ' + this.avgSalesPerHour[i] + ' cookies');
+    console.log(`${this.hoursOfOperation[i]}: ${this.avgSalesPerHour[i]} cookies`);
+
   }
-  console.log('Total Cookies: ' + this.totalSales());
+  // console.log('Total Cookies: ' + this.totalSales());
+  console.log(`Total Cookies: ${this.totalSales()}`);
+
 }
 
 //Create a function to calculate average sales per hour ----> avgSalesPerHour = avgCookiePerCustomer * numCustomersArray[i]
@@ -109,10 +184,25 @@ function renderResultsToPage(id) {
     nextUl.appendChild(nextLi);
   }
   var totalLi = document.createElement('li');
-  totalLi.textContent = 'Total Cookies: ' + this.totalSales();
+  totalLi.textContent = `Total Cookies: ${this.totalSales()}`;
   nextUl.appendChild(totalLi);
 }
 
 //Initialize Page
 pike.listEverything();
 pike.render('pike');
+
+seatacAirport.listEverything();
+seatacAirport.render('seatacAirport');
+
+seattleCenter.listEverything();
+seattleCenter.render('seattleCenter');
+
+capHill.listEverything();
+capHill.render('capHill');
+
+alki.listEverything();
+alki.render('alki');
+
+
+
